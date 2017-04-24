8fa2f3a11ee9507fd0486daf109e409904592636
John Daly <jpdaly@gmail.com>
2017-04-24 11:10:55 +0100

    fixture object sets own unix time

--- scrapr/client/src/models/specs/fixture_spec.js
+++ scrapr/client/src/models/specs/fixture_spec.js
@@ -2,11 +2,18 @@ const assert = require('assert');
 const Fixture = require('../Fixture');
 
 describe('fixture', () => {
-  let fixture;
+  var fixture;
 
   beforeEach(() => {
     fixture = new Fixture({
       date: 'Monday 24th April 2017',
+
+      time: '18:45'
+    });
+
+    fixture2 = new Fixture({
+      date: 'Monday 24th April 2017',
+
       time: '18:45'
     })
   });
@@ -16,11 +23,19 @@ describe('fixture', () => {
     assert.equal('18:45', fixture.time);
   });
 
-  it('joins date and time into a string');
+  it('joins date and time into a string', () => {
+    assert.equal('Monday 24th April 2017 18:45', fixture.getLongString());
+  });
 
-  it('breaks date and time into parseable elements');
+  it('breaks date and time into parseable elements', () => {
+    assert.equal(fixture.getParseableTimeObject(), '24-April-2017-18:45');
+  });
 
-  it('generates unix time from elements');
+  it('generates unix time from elements', () => {
+    assert.equal(fixture.generateUnixTime(), 1493055900);
+  });
 
-  it('sets a unixTime element when instatiated');
+  it('sets a unixTime element when instatiated', () => {
+    assert.equal(fixture2.unixTime, 1493055900);
+  });
 })
