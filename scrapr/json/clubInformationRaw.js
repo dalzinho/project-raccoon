049ef55cc07f283b2bcd7f38e6b832d83ad96dc0
John Daly <jpdaly@gmail.com>
2017-04-28 10:34:11 +0100

    step towards serving recent tweets

--- scrapr/json/clubInformationRaw.js
+++ scrapr/json/clubInformationRaw.js
@@ -1,41 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-let clubs = [{
-  fullName: "Pollok",
-  prettyName: "Pollok",
-  homeGround: {
-    groundName: "Newlandsfield",
-    groundTown: "Glasgow",
-    groundPostcode: "G432XR",
-  },
-  crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444ac5/1349467457473/1000w/10576389-20537505-thumbnail.jpg",
-  twitter: "@pollokfc",
-  website: "http://www.pollokfc.com"
-},
+let clubs = [
+
 {
-  fullName: "Kirkintilloch Rob Roy",
-  prettyName: "KRR",
+  fullName: "Arthurlie",
+  prettyName: "Arthurlie",
   homeGround: {
-    groundName: "Guy's Meadow",
-    groundTown: "Cumbernauld",
-    groundPostcode: "G67",
+    groundName: "Dunterlie Park",
+    groundTown: "Barrhead",
+    groundPostcode: "PA91",
   },
-  crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444aac/1349466636243/1000w/10576389-20537546-thumbnail.jpg",
-  twitter: "@robroy1878",
-  website: "http://www.robroy.com"
-},
-{
-  fullName: "Kilwinning Rangers",
-  prettyName: "Kilwinning",
-  homeGround: {
-    groundName: "Abbey Park",
-    groundTown: "Kilwinning",
-    groundPostcode: "KA136DU",
-  },
-  crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444aaa/1349466586068/1000w/10576389-20537545-thumbnail.jpg",
-  twitter: "@steadythebuffs",
-  website: "null"
+  crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444a79/1349463168137/1000w/10576389-12423976-thumbnail.jpg",
+  twitter: "@arthurliefc",
+  website: "http://www.arthurlie.com"
 },
 {
   fullName: "Auchinleck Talbot",
@@ -50,6 +28,30 @@ let clubs = [{
   website: "null"
 },
 {
+  fullName: "Beith Juniors",
+  prettyName: "Beith",
+  homeGround: {
+    groundName: "Bellsdale Park",
+    groundTown: "Beith",
+    groundPostcode: "KA152AF",
+  },
+  crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444a7f/1349461648023/1000w/10576389-13623211-thumbnail.jpg",
+  twitter: "@beithjuniorsfc",
+  website: "http://www.beithjuniors.com"
+},
+{
+  fullName: "Cumnock Juniors",
+  prettyName: "Cumnock",
+  homeGround: {
+    groundName: "Townhead Park",
+    groundTown: "Cumnock",
+    groundPostcode: "KA181LZ",
+  },
+  crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444a8c/1318722139627/1000w/Cumnock.jpg",
+  twitter: "@cumnockjuniorsfc",
+  website: "http://www.cumnockjuniors.co.uk"
+},
+{
   fullName: "Glenafton Athletic",
   prettyName: "Glenafton",
   homeGround: {
@@ -60,12 +62,97 @@ let clubs = [{
   crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444a98/1349461881293/1000w/10576389-15546177-thumbnail.jpg",
   twitter: "@flowgently",
   website: "null"
-}
+},
+{
+  fullName: "Hurlford United",
+  prettyName: "Hurlford",
+  homeGround: {
+    groundName: "Blair Park",
+    groundTown: "Hurlford",
+    groundPostcode: "KA34",
+  },
+  crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444a9c/1349468482743/1000w/10576389-20538201-thumbnail.jpg",
+  twitter: null,
+  website: "http://www.hurlford.com"
+},
+{
+  fullName: "Kilbirnie Ladeside",
+  prettyName: "Kilbirnie",
+  homeGround: {
+    groundName: "Valefield Park",
+    groundTown: "Kilbirnie",
+    groundPostcode: "KA136DU",
+  },
+  crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444aa6/1349466460007/1000w/10576389-20537543-thumbnail.jpg",
+  twitter: "@ladeside1901",
+  website: "null"
+},
+
+{
+  fullName: "Kilwinning Rangers",
+  prettyName: "Kilwinning",
+  homeGround: {
+    groundName: "Abbey Park",
+    groundTown: "Kilwinning",
+    groundPostcode: "KA136DU",
+  },
+  crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444aaa/1349466586068/1000w/10576389-20537545-thumbnail.jpg",
+  twitter: "@steadythebuffs",
+  website: "null"
+},
+{
+  fullName: "Kirkintilloch Rob Roy",
+  prettyName: "KRR",
+  homeGround: {
+    groundName: "Guy's Meadow",
+    groundTown: "Cumbernauld",
+    groundPostcode: "G67",
+  },
+  crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444aac/1349466636243/1000w/10576389-20537546-thumbnail.jpg",
+  twitter: "@robroy1878",
+  website: "http://www.robroy.com"
+},
+{
+  fullName: "Largs Thistle",
+  prettyName: "Largs",
+  homeGround: {
+    groundName: "Barrfields Park",
+    groundTown: "Largs",
+    groundPostcode: "KA308NP",
+  },
+  crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444ab0/1349467037837/1000w/10576389-20537513-thumbnail.jpg",
+  twitter: "@largsthistlefc",
+  website: "http://www.largsthistle.org.uk"
+},
+{
+  fullName: "Pollok",
+  prettyName: "Pollok",
+  homeGround: {
+    groundName: "Newlandsfield",
+    groundTown: "Glasgow",
+    groundPostcode: "G432XR",
+  },
+  crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444ac5/1349467457473/1000w/10576389-20537505-thumbnail.jpg",
+  twitter: "@pollokfc",
+  website: "http://www.pollokfc.com"
+},
+{
+  fullName: "Troon",
+  prettyName: "Troon",
+  homeGround: {
+    groundName: "Portland Park",
+    groundTown: "Troon",
+    groundPostcode: "KA99",
+  },
+  crestUrl: "https://static1.squarespace.com/static/552138ebe4b0abe69be47a2c/55b234a2e4b03d4ada444a70/55b234a2e4b03d4ada444add/1349467988008/1000w/10576389-20537494-thumbnail.jpg",
+  twitter: "@troon_fc",
+  website: "http://www.troonfc.com"
+},
 ]
 
 fs.writeFile(path.join('clubInformation.json'), JSON.stringify(clubs), (error) =>{
   if(error){ console.log('Error:', error)}
-    else {console.log('clubInfo written to json folder')}
+    else {console.log(`Information on ${clubs.length} clubs written to json folder`)}
   })
 
 // module.exports = clubs;
--- server.js
+++ server.js
@@ -2,14 +2,39 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const Twitter = require('twitter');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
 
 
 app.set('port', (process.env.PORT || 3001));
+app.use(bodyParser.json());
 
 // if(process.env.NODE_ENV === 'production'){
   // app.use(express.static('client/build'));
 // }
 
+app.get('/api/twitter', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://www.example.com:3000');
+
+  var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+
+  var params = {screen_name: 'pollokfc'};
+  
+  client.get('statuses/user_timeline', params, (error, tweets, response) => {
+    if (!error) {
+      res.send(tweets);
+    } else {
+      console.error(error);
+    }
+  })
+});
 
 app.get('/api/table', (req, res) => {
   res.header('Access-Control-Allow-Origin', 'http://www.example.com:3000');
