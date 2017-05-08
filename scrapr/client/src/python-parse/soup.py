6836f211bfe4ea5f3ae0295b67e2a41621ae4fe3
John Daly <jpdaly@gmail.com>
2017-05-08 11:37:22 +0100

    parser refactoed slightly to work better with all competitions. save point while sure league fixtures work :)

--- scrapr/client/src/python-parse/soup.py
+++ scrapr/client/src/python-parse/soup.py
@@ -6,27 +6,37 @@ with open("sjfadata.html") as fp:
 	soup = BeautifulSoup(fp, "lxml")
 
 tables = soup.find_all("div", class_='compTable')
-leagueTable = tables[0].find_all("div", class_='compTableRow')
+leagueTable = tables[0]
 
 matches = []
+
 
 # iterate through compTable, take id from this rather than competition stringy name?
 
 def scrape_data(table):
-	competition = table[0].find("div", class_='compTableHead').text
-	for i in range (1, len(table)):
-		match = table[i]
-		matches.append({
-			"competition": competition,
-			"date":match.find("div", class_='cellDate').text,	
-			"ko":match.find("div", class_='cellKO').text,
-			"home":match.find("div", class_='cellClubHome').text,
-			"homeScore":match.find_all("div", class_='cellScore')[0].text,
-			"away":match.find("div", class_='cellClubAway').text,
-			"awayScore":match.find_all("div", class_='cellScore')[1].text,
-			"venue": match.find("div", class_='cellVenue').text
+	# the overall table id, refers directly to competition
+	competition = table.attrs['id']
 
-		}),
+	# returns matchtable - this has round information helpfully removed
+	matchTables = leagueTable.find_all('div', class_='matchTable')
+	for matchTable in matchTables:
+		# now get the compRows; these contain the actual fixture elements
+		allMatchesInTable = matchTable.find_all('div', class_='compTableRow')
+
+		for match in allMatchesInTable:
+		
+			matches.append({
+				"competition": competition,
+				"date":match.find("div", class_='cellDate').text,	
+				"ko":match.find("div", class_='cellKO').text,
+				"home":match.find("div", class_='cellClubHome').text,
+				"homeScore":match.find_all("div", class_='cellScore')[0].text,
+				"away":match.find("div", class_='cellClubAway').text,
+				"awayScore":match.find_all("div", class_='cellScore')[1].text,
+				"venue": match.find("div", class_='cellVenue').text,
+				"center": match.find("div", class_='cellCenter').text	
+
+			}),
 
 
 
